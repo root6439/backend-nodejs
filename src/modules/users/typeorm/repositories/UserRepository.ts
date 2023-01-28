9fb76b2c8674800aebc1a74550823b71b495e1c9
Nicolas Soares Henrique <nicolas.henrique@capgemini.com>
2023-01-28 20:41:34 -0300

    user repository

---
+++ src/modules/users/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,15 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    return await this.findOne({ where: { name } });
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return await this.findOne({
+      where: email,
+    });
+  }
+}
